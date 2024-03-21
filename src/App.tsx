77c3d59ceb406f89c83ffbbae8bc9dc7765d0683
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-21 22:00:57 +0000

    pass the option selected on my DropdownMenu to my CityWeather

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { DropdownMenu } from './components/DropdownMenu';
 import { DropdownMenuProvider } from './components/contexts/DropdownMenuContext';
+import { CityWeather } from './components/CityWeather';
+import { useState } from 'react';
 
 function App() {
+  const [selectedCity, setSelectedCity] = useState<string>('Lisbon');
+
+  const handleCityChange = (city: string) => {
+    setSelectedCity(city);
+  };
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
       <DropdownMenuProvider>
         <div className="App">
           <Header title="Weather app"/>
-          <DropdownMenu />
+          <DropdownMenu onCityChange={handleCityChange} selectedCity={selectedCity}/>
+          <CityWeather city={selectedCity} />
         </div>
       </DropdownMenuProvider>
     </ThemeProvider>
